ba3b765697ce7ed4fb09113de83dde234fb08f23
DinoXanh <mumcip99@gmail.com>
2026-06-16 19:03:51 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,20 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio của Nguyễn Văn A đã load xong!');
     alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
 });
+
+const observer =
+new IntersectionObserver(entries=>{
+
+entries.forEach(entry=>{
+
+if(entry.isIntersecting){
+entry.target.classList.add('show');
+}
+
+});
+
+});
+
+document
+.querySelectorAll('.hidden')
+.forEach(el=>observer.observe(el));
