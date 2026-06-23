e05916c7c8540c06a154c1d00a728e415e4cc68e
DinoXanh <mumcip99@gmail.com>
2026-06-23 21:13:50 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Portfolio của Nguyễn Tấn Phát đã load xong!');
-    alert('Chào mừng bạn đến với portfolio thật trên Internet của mình!');
-});
-
-const observer =
-new IntersectionObserver(entries=>{
-
-entries.forEach(entry=>{
-
-if(entry.isIntersecting){
-entry.target.classList.add('show');
-}
-
-});
-
-});
-
-document
-.querySelectorAll('.hidden')
-.forEach(el=>observer.observe(el));
-
-const btn =
-document.getElementById("themeBtn");
-
-btn.addEventListener("click",()=>{
-
-document.body.classList.toggle("dark");
-
+document.addEventListener(&#39;DOMContentLoaded&#39;, () =&gt; {
+console.log(&#39;Portfolio của Nguyễn Văn A đã load xong!&#39;);
+alert(&#39;Chào mừng bạn đến với portfolio thật trên Internet của mình!&#39;);
 });
